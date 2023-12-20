816ffb53637c39fbe00ac27b90e5a962b15a7c5b
ys-grouse <malsawma7777@gmail.com>
2023-12-20 14:06:20 +0530

    chore[update]: api endpoint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 const mix = require("laravel-mix");
 
 mix.copy(
-    "../frontend-tilte-storage/dist/pwa/index.html",
+    "../frontend-storage/dist/pwa/index.html",
     "resources/views/app.blade.php"
 )
-    .copyDirectory("../frontend-tilte-storage/dist/pwa", "public")
-    .copyDirectory("../frontend-tilte-storage/src/assets", "public/src/assets");
+    .copyDirectory("../frontend-storage/dist/pwa", "public")
+    .copyDirectory("../frontend-storage/src/assets", "public/src/assets");
