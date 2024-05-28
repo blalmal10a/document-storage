7362ba1b40dbef66a7879a4ae2d52e4453e849e2
ys-grouse <malsawma7777@gmail.com>
2024-05-28 15:53:24 +0530

    change path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 const mix = require("laravel-mix");
 
 mix.copy(
-    "../frontend-storage/dist/pwa/index.html",
+    "../frontend-siruk-storage/dist/pwa/index.html",
     "resources/views/app.blade.php"
 )
-    .copyDirectory("../frontend-storage/dist/pwa", "public")
-    .copyDirectory("../frontend-storage/src/assets", "public/src/assets");
+    .copyDirectory("../frontend-siruk-storage/dist/pwa", "public")
+    .copyDirectory("../frontend-siruk-storage/src/assets", "public/src/assets");
